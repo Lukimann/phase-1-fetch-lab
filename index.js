4ec91e6e8cdb64b43c4a8e0c79b2f307917b3f8d
Luka Wanyama <lukewanyama5@gmail.com>
2024-01-08 13:45:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  return fetch('https://anapioficeandfire.com/api/characters/583')
+  return fetch('https://anapioficeandfire.com/api/books')
   .then((response) => {
     return response.json;
   }) .then((data) => {
